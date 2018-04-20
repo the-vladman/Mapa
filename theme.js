5d591428d1675a37baa78232b3bc342bb4c9fe0f
Omar Vladimir Galicia Palmeros <thewakosmanclub@gmail.com>
2018-04-19 19:16:41 -0500

    Adaptacion de Grafica Base

--- theme.js
+++ theme.js
@@ -1,9 +1,7 @@
-import {cyan700, lime500} from 'material-ui/styles/colors';
-
 export default {
   palette: {
-    primary1Color: cyan700,
-    active1Color: lime500,
-    accent1Color: cyan700
+    primary1Color: '#393c3e',
+    active1Color: '#f66',
+    accent1Color: '#393c3e'
   }
 };
