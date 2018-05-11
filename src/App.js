5334521189b6143650eef24a951aa445b8c584a3
Omar Vladimir Galicia Palmeros <thewakosmanclub@gmail.com>
2018-05-11 11:34:01 -0500

    Add Control Buttons

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
 import React, { Component } from 'react';
 import './App.css';
 import ol from 'openlayers';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import CustomTheme from './theme';
 import LoadingPanel from '@boundlessgeo/sdk/components/LoadingPanel';
 import MapPanel from '@boundlessgeo/sdk/components/MapPanel';
+import Globe from '@boundlessgeo/sdk/components/Globe';
+import HomeButton from '@boundlessgeo/sdk/components/HomeButton';
+import Geolocation from '@boundlessgeo/sdk/components/Geolocation';
+import Zoom from '@boundlessgeo/sdk/components/Zoom';
+import Rotate from '@boundlessgeo/sdk/components/Rotate';
+import Measure from '@boundlessgeo/sdk/components/Measure';
+import Navigation from '@boundlessgeo/sdk/components/Navigation';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+
+// Needed for onTouchTap
+// Can go away when react 1.0 release
+// Check this repo:
+// https://github.com/zilverline/react-tap-event-plugin
+injectTapEventPlugin();
 
 var map = new ol.Map({
   layers: [
@@ -22,20 +38,37 @@ var map = new ol.Map({
   view: new ol.View({
     projection: "EPSG:4326",
     center: [-101.9563, 23.6257],
-    zoom: 5
+    zoom: 6
   })
 });
 
 class App extends Component {
+  getChildContext() {
+    return {
+      muiTheme: getMuiTheme(CustomTheme)
+    };
+  }
+
   render() {
     return (
       <div className="App">
-        <h1 className="App-title">Soy el Mapa</h1>
         <MapPanel map={map} />
         <LoadingPanel map={map} />
+        <div id='control-buttons'>
+          <div id='control-button'><Globe map={map} /></div>
+          <div id='control-button'><Geolocation map={map} /></div>
+          <div id='control-button'><HomeButton map={map} /></div>
+          <div id='control-button'><Zoom map={map} /></div>
+          <div id='control-button'><Rotate map={map} /></div>
+        </div>
       </div>
     );
   }
 }
+
+App.childContextTypes = {
+  muiTheme: React.PropTypes.object
+};
+
 
 export default App;
---
+++ src/theme.js
@@ -0,0 +1,7 @@
+export default {
+    palette: {
+        primary1Color: '#393c3e',
+        active1Color: '#f66',
+        accent1Color: '#393c3e'
+    }
+};
