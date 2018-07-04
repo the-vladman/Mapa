1202807342dd95ced8d63a7ef02217a07bc0c553
Omar Vladimir Galicia Palmeros <thewakosmanclub@gmail.com>
2018-07-04 17:51:50 -0500

    update layersControl

--- src/components/MapaLayersControl.js
+++ src/components/MapaLayersControl.js
@@ -1,14 +1,27 @@
 import React, { Component } from 'react';
 import './MapaLayersControl.css';
 import Paper from 'material-ui/Paper';
+import { List, ListItem } from 'material-ui/List';
 
 class MapaLayersControl extends Component {
     render() {
-        return(
-          <div className='layers-control-container'>
-            <Paper elevation={5}>HOLA MUNO</Paper>
-          </div>
-        );
+      const layers = this.props.mapa.getLayers();
+      return(
+      <div className='layers-control-container'>
+        <Paper elevation={5}>
+            <div id='layers-control-header'>
+              <div><h1 id='layers-control-text'>Capas</h1></div>
+            </div>
+            <div>
+              <List>
+                <ListItem primaryText="Inbox"/>
+                <ListItem primaryText="Inbox"/>
+                <ListItem primaryText="Inbox"/>
+              </List>
+            </div>
+        </Paper>
+      </div>
+      );
     }
 }
 
