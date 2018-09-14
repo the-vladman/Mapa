aeece46c4ac8ef1900a7865a34a90ab990a7730a
Omar Vladimir Galicia Palmeros <thewakosmanclub@gmail.com>
2018-09-13 21:41:43 -0500

    add Router
    add react-router y query-string to get params for configuration

--- src/index.js
+++ src/index.js
@@ -5,9 +5,10 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import enMessages from '@boundlessgeo/sdk/locale/en';
 import { IntlProvider } from 'react-intl';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 var locale = 'en';
 var i18n = enMessages;
 
-ReactDOM.render(<IntlProvider locale={locale} messages={i18n}><App /></IntlProvider>, document.getElementById('root'));
+ReactDOM.render(<IntlProvider locale={locale} messages={i18n}><BrowserRouter><Route path={'/'} component={App} /></BrowserRouter></IntlProvider>, document.getElementById('root'));
 registerServiceWorker();
