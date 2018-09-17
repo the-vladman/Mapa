d19b53b6a2fc678d653c98f1e5c360e83f89b2c0
Omar Vladimir Galicia Palmeros <thewakosmanclub@gmail.com>
2018-09-17 17:47:49 -0500

    new component created

---
+++ src/components/MapaPopUp.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import './MapaPopUp.css';
+import ol from 'openlayers';
+
+class MapaPopUp extends Component {
+    constructor(props) {
+        super(props);
+        props.mapa.on('singleclick', this.onMapClick);
+        this.state = {
+            popupTexts: []
+        };
+    }
+
+    onMapClick = (evt) =>{
+        var pixel = this.props.mapa.getEventPixel(evt.originalEvent);
+        this.props.mapa.forEachFeatureAtPixel(pixel, function(feature, layer) {
+            console.log('features', feature.getProperties())
+        })
+    };
+
+    render() {  
+        return null;
+    }
+}
+
+export default MapaPopUp;
